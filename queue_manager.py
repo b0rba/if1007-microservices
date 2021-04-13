aad36ff6c7f6a9538ed346e405a4cb90855e59b0
Vitor <vitormatheus85@gmail.com>
2021-04-13 11:15:20 -0300

    feat: create dummy producer

--- queue_manager.py
+++ queue_manager.py
@@ -2,6 +2,8 @@ import pika
 
 
 class QueueManager:
+    QUEUE_NAME = 'queue'
+
     def __init__(self, url: str):
         params = pika.URLParameters(url)
         self.connection = pika.BlockingConnection(params)
